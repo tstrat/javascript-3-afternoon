0562346be982851b57838215e2427f1e491dff80
tstrat <tstrat@email.arizona.edu>
2018-09-26 15:24:52 -0700

    Complete work for classess.js

--- classes.js
+++ classes.js
@@ -30,7 +30,18 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -50,7 +61,27 @@
 */
 
 //Code Here
+class Manager {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+  }
 
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+
+  hire(employee) {
+    this.reports.push(employee)
+  }
+
+  fire(index) {
+    this.reports.splice(index,1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -76,6 +107,36 @@
 */
 
 //Code Here
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+  
+  hire(employee) {
+    this.reports.push(employee)
+    this.title = this.reports.length < 1 ? 'Not a manager' :
+                this.reports.length < 4 ? 'Barely Manager' :
+                this.reports.length < 11 ? 'Mostly Manager' :
+                this.reports.length < 51 ? 'Manager' :
+                this.reports.length < 101 ? 'Manager Plus' : 'Bestest Manager'
+                
+  }
+
+  fire(index) {
+    this.reports.splice(index,1)
+    this.bonus += 100
+  }
+}
 
 
 
@@ -104,4 +165,26 @@
 
 //Code Here
 
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
 
+  makeWidgets(num) {
+    this.widgets_made_count += num
+    this.wear_and_tear_count = Math.floor(this.widgets_made_count / 50)
+  }
+
+  fixMachine() {
+    this.needs_reboot = true
+  }
+
+  reboot() {
+    return () => {
+    this.wear_and_tear_count-=10
+    this.needs_reboot = false
+    }
+  }
+}
